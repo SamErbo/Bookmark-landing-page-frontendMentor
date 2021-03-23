ec97f88030fd316daaa35b058c5fa8862174cf73
SamErbo <samuel.ermias@gmail.com>
2021-03-23 11:54:39 +0300

    initial commit

---
+++ app/js/script.js
@@ -0,0 +1,8 @@
+const ham = document.querySelector("#hamburger");
+
+const hicon = document.querySelector("#h-icon");
+
+ham.addEventListener('click', function(){
+    hicon.src="./images/icon-close.svg";
+
+});
